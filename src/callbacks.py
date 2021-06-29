743fba6196a01985972bff75c1ec9a63e3f672f7
es33333333 <dknee.nhs@icloud.com>
2021-06-29 22:07:03 +0900

    update callbacks.py

--- src/callbacks.py
+++ src/callbacks.py
@@ -4,7 +4,7 @@ from app import app
 from layouts import default_stylesheet, universe_stylesheet, flower_stylesheet
 
 
-# ドロップダウンでスタイルを変更し、マウスをかざしたノードの機種名を表紙する
+# ドロップダウンでスタイルを変更し、マウスをかざしたノードの機種名を表示する
 @app.callback(
     Output('graph', 'stylesheet'),  # グラフ部分のスタイル
     Output('html', 'style'),  # ページ全体のスタイル
@@ -25,17 +25,19 @@ def update_graph(theme, node_data_dict):  # Inputの値が引数になる。
 
     if node_data_dict:  # ノードにマウスがかざされたとき
         # ノードの枠線およびラベル表示変更(機種名表示)
-        style_with_label = {'selector': '#' + node_data_dict['id'],
-                            'style': {
-                                'label': (node_data_dict['model']),
-                                'border-width': 5,
-                                'border-style': 'double',
-                                'border-color': '#8b008b',
-                                'border-opacity': 0.9}}
+        style_with_label = [{'selector': '#' + node_data_dict['id'],
+                             'style': {
+                                 'label': (node_data_dict['model']),
+                                 'border-width': 5,
+                                 'border-style': 'double',
+                                 'border-color': '#8b008b',
+                                 'border-opacity': 0.9}}]
 
-        updated_stylesheet = selected_stylesheet + [style_with_label]
+        updated_stylesheet = selected_stylesheet + style_with_label
 
     else:
         updated_stylesheet = selected_stylesheet
-
+    # データが取れてるか
+    # print(node_data_dict['model'])
+    print(updated_stylesheet)
     return updated_stylesheet, html_style
