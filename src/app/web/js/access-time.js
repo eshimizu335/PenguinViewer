39d197bb0815382a6cba97d51d4318b111e193ea
eshimizu335 <dknee.nhs@icloud.com>
2022-03-16 22:41:27 +0900

    modify access-time.js

--- src/app/web/js/access-time.js
+++ src/app/web/js/access-time.js
@@ -4,6 +4,6 @@ window.addEventListener("DOMContentLoaded", async function getAccessTime(){
     let accessTime = await eel.get_access_time()();
     let accessTimeText = document.createTextNode(accessTime["access_year"]+"年"+accessTime["access_month"]+"月"+accessTime["access_date"]+"日"+accessTime["access_hour"]+"時"+accessTime["access_minute"]+"分のネットワーク図");
     newH2.appendChild(accessTimeText);
-    let graphDiv = document.getElementById("graph");
-    graphDiv.parentNode.insertBefore(newH2, graphDiv);
+    let timeDiv = document.getElementById("access_time");
+    timeDiv.parentNode.insertBefore(newH2, timeDiv);
     })
